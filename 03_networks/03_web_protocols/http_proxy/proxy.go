8980beef3241f1178c65eab6bdf2764939d6f4d7
Alex Bozhenko <alexbozhenko@gmail.com>
2021-07-08 18:09:05 +0300

    add parsing of http request usin g http library

--- 03_networks/03_web_protocols/http_proxy/proxy.go
+++ 03_networks/03_web_protocols/http_proxy/proxy.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"net/http"
 	"sync"
 
 	"golang.org/x/sys/unix"
 )
+
+const HTTP_HEADER_DELIMETER string = "\r\n\r\n"
 
 func handleError(err error) {
 	if err != nil {
@@ -13,19 +18,21 @@ func handleError(err error) {
 	}
 }
 
-func handleClient(listeningSocketFD int, waitGroup *sync.WaitGroup) {
+func handleClient(proxyListeningSocketFD int, waitGroup *sync.WaitGroup) {
 	defer waitGroup.Done()
 	fmt.Printf("waitGroup: %v\n", waitGroup)
 
-	connectionSocketFD, clientSockAddr, err := unix.Accept(listeningSocketFD)
-	defer unix.Close(connectionSocketFD)
+	proxyIncomingConnectionSocketFD, clientSockAddr, err := unix.Accept(proxyListeningSocketFD)
+	defer unix.Close(proxyIncomingConnectionSocketFD)
 	handleError(err)
 
 	// As soon as connection was accepted by listen(),
 	// we just create another goroutine that will block on listen(),
-	// waiting for next connection to accept
+	// waiting for next connection to accept.
+	// So we have unbounded parallelism, serving as many clients as
+	// operating system limits allow
 	waitGroup.Add(1)
-	go handleClient(listeningSocketFD, waitGroup)
+	go handleClient(proxyListeningSocketFD, waitGroup)
 
 	destinationSockAddr := &unix.SockaddrInet4{
 		Port: 9000,
@@ -40,9 +47,10 @@ func handleClient(listeningSocketFD int, waitGroup *sync.WaitGroup) {
 
 	// same size as sysctl net.core.rmem_max
 	buf := make([]byte, 212992)
+	sendBuffer := []byte{}
 
 	for {
-		nBytesRead, _, err := unix.Recvfrom(connectionSocketFD, buf, 0)
+		nBytesRead, _, err := unix.Recvfrom(proxyIncomingConnectionSocketFD, buf, 0)
 		handleError(err)
 		if nBytesRead == 0 {
 			// according to man 2 recv,
@@ -50,19 +58,31 @@ func handleClient(listeningSocketFD int, waitGroup *sync.WaitGroup) {
 			// file" return).
 			break
 		}
-		fmt.Printf("%s", buf[:nBytesRead])
-		err = unix.Sendto(destinationSocketFD, buf[:nBytesRead], 0, clientSockAddr)
-		handleError(err)
+		// having tcp a stream of bytes, we collect the stream to sendBuffer
+		// until we found http headers delimeter
+		// (assuming that we have a GET request, we have no body)
+		if delimeterIndex := bytes.LastIndex(buf[:nBytesRead],
+			[]byte(HTTP_HEADER_DELIMETER)); delimeterIndex == -1 {
+			sendBuffer = append(sendBuffer, buf[:nBytesRead]...)
+		} else {
+			sendBuffer = append(sendBuffer, buf[:delimeterIndex+len(HTTP_HEADER_DELIMETER)]...)
+			request, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(sendBuffer)))
+			handleError(err)
+			fmt.Println(request)
+			err = unix.Sendto(destinationSocketFD, sendBuffer, 0, clientSockAddr)
+			handleError(err)
+			sendBuffer = buf[delimeterIndex+len(HTTP_HEADER_DELIMETER) : nBytesRead]
+		}
 	}
 
 }
 
 func main() {
 	var wg sync.WaitGroup
-	listeningSocketFD, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
-	defer unix.Close(listeningSocketFD)
+	proxyListeningSocketFD, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
+	defer unix.Close(proxyListeningSocketFD)
 	handleError(err)
-	err = unix.Bind(listeningSocketFD, &unix.SockaddrInet4{
+	err = unix.Bind(proxyListeningSocketFD, &unix.SockaddrInet4{
 		Port: 8000,
 		Addr: [4]byte{0, 0, 0, 0},
 	})
@@ -71,16 +91,16 @@ func main() {
 	// Let's close the socket using RST, so we do not have to wait for sockets in
 	// TIME_WAIT to expire when the program is closed, and we want to restart
 	// immediately.
-	err = unix.SetsockoptLinger(listeningSocketFD, unix.SOL_SOCKET, unix.SO_LINGER,
+	err = unix.SetsockoptLinger(proxyListeningSocketFD, unix.SOL_SOCKET, unix.SO_LINGER,
 		&unix.Linger{
 			Onoff:  1,
 			Linger: 0,
 		})
 	handleError(err)
 
-	err = unix.Listen(listeningSocketFD, 1)
+	err = unix.Listen(proxyListeningSocketFD, 1)
 	handleError(err)
 	wg.Add(1)
-	go handleClient(listeningSocketFD, &wg)
+	go handleClient(proxyListeningSocketFD, &wg)
 	wg.Wait()
 }
