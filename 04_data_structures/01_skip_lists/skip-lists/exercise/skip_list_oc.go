d958b76217d227c83b3dd76a0800cb55d644ca6a
Alex Bozhenko <alexbozhenko@gmail.com>
2021-07-11 15:50:49 +0300

    Add print function for skip list

---
+++ 04_data_structures/01_skip_lists/skip-lists/exercise/skip_list_oc.go
@@ -0,0 +1,77 @@
+package main
+
+import "fmt"
+
+type skipListNode struct {
+	item    Item
+	forward []*skipListNode
+}
+
+func (node skipListNode) Level() int {
+	return len(node.forward)
+}
+
+type skipListOC struct {
+	head []*skipListNode
+}
+
+func (list skipListOC) Level() int {
+	return len(list.head)
+}
+
+func newSkipListOC() *skipListOC {
+	return &skipListOC{
+		head: []*skipListNode{
+			nil,
+		},
+	}
+}
+
+func (list skipListOC) String() string {
+	result := ""
+	for i := 0; i < list.Level(); i++ {
+		result += fmt.Sprintf("Level %v:\n", i+1)
+		for currentNode := list.head[i]; currentNode !=
+			nil && i < currentNode.Level(); currentNode = (*currentNode).forward[i] {
+			result += fmt.Sprintf(
+				"%s: %s ->", currentNode.item.Key, currentNode.item.Value)
+		}
+
+		result += "\n"
+	}
+	return result
+}
+
+func (o *skipListOC) Get(key string) (string, bool) {
+	return "", false
+}
+
+func (o *skipListOC) Put(key, value string) bool {
+	return false
+}
+
+func (o *skipListOC) Delete(key string) bool {
+	return false
+}
+
+func (o *skipListOC) RangeScan(startKey, endKey string) Iterator {
+	return &skipListOCIterator{}
+}
+
+type skipListOCIterator struct {
+}
+
+func (iter *skipListOCIterator) Next() {
+}
+
+func (iter *skipListOCIterator) Valid() bool {
+	return false
+}
+
+func (iter *skipListOCIterator) Key() string {
+	return ""
+}
+
+func (iter *skipListOCIterator) Value() string {
+	return ""
+}
