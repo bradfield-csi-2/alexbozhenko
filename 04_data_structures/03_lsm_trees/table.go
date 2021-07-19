bf1ca2224be357dfeffe2089e8dc41594461385c
Alex Bozhenko <alexbozhenko@gmail.com>
2021-07-19 18:08:29 +0300

    update desing to store offsets to values

--- 04_data_structures/03_lsm_trees/table.go
+++ 04_data_structures/03_lsm_trees/table.go
@@ -5,7 +5,33 @@ type Item struct {
 }
 
 // Given a sorted list of key/value pairs, write them out according to the format you designed.
-func Build(path string, sortedItems []Item) error {
+func Build(filePath string, sortedItems []Item) error {
+	// Based on the idea in this answer:
+	// https://dba.stackexchange.com/a/11190
+
+	// File format is the fowllowing:
+	// +--------------+
+	// | 3            | number of entries (4 bytes)
+	// +--------------+
+	// | 16           | offset of first key data (4 bytes)
+	// +--------------+
+	// | 24           | offset of second key data (4 bytes)
+	// +--------------+
+	// | 39           | offset of third key data (4 bytes)
+	// +--------------+-----------------+
+	// | key one | data offset(4 bytes) |
+	// +----------------+----------------------+
+	// | key number two | data offset(4 bytes) |
+	// +-----------------------+----------------------+
+	// | this is the third key | data offset(4 bytes) |
+	// +-----------------------+----------------------+
+	// +-----------+
+	// | value one |
+	// +-----------+-------+
+	// | value for key two |
+	// +-------------------------+
+	// | this is the third value |
+	// +-------------------------+
 	return nil
 }
 
@@ -15,12 +41,17 @@ func Build(path string, sortedItems []Item) error {
 // Although a Table shouldn't keep all the key/value data in memory, it should contain
 // some metadata to help with efficient access (e.g. size, index, optional Bloom filter).
 type Table struct {
-	// TODO
+	// Design of the file format requires us to load entire list of
+	// keys into memory, so we can search quickly, which is suboptiomal.
+	// Fix in next iteration?
+
+	// skip list of keys to data offsets
+	// file descriptor? pointer? ...
 }
 
 // Prepares a Table for efficient access. This will likely involve reading some metadata
 // in order to populate the fields of the Table struct.
-func LoadTable(path string) (*Table, error) {
+func LoadTable(filePath string) (*Table, error) {
 	return nil, nil
 }
 
