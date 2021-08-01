9cee822f93eee3567af2916de184a6aa2a4c0f48
Alex Bozhenko <alexbozhenko@gmail.com>
2021-08-01 12:15:47 +0300

    add get and set with pre-allocated bitmap for uint32 range of value

---
+++ 04_data_structures/07_roaring_bitmaps/go.mod
@@ -0,0 +1,5 @@
+module roaring_bitmaps
+
+go 1.16
+
+require go.uber.org/zap v1.18.1
--- 04_data_structures/07_roaring_bitmaps/uncompressed.go
+++ 04_data_structures/07_roaring_bitmaps/uncompressed.go
@@ -1,25 +1,41 @@
 package bitmap
 
+import (
+	"go.uber.org/zap"
+)
+
 const wordSize = 64
 
 type uncompressedBitmap struct {
 	data []uint64
 }
 
+var logger, _ = zap.NewProduction()
+var sugar = logger.Sugar()
+
 func newUncompressedBitmap() *uncompressedBitmap {
-	return &uncompressedBitmap{}
+	// Range for uint32 is [0; 2**32-1], so we need
+	// 2**32 bits, or 2**29 bytes, or 2**26 = 67108864 8-bytes blocks
+	return &uncompressedBitmap{
+		data: make([]uint64, 1<<26),
+	}
 }
 
 func (b *uncompressedBitmap) Get(x uint32) bool {
-	return false
+	blockNumber := x / wordSize
+	bitPositionInBlock := wordSize - (x % wordSize) - 1
+	return (b.data[blockNumber]&(1<<bitPositionInBlock) != 0)
 }
 
 func (b *uncompressedBitmap) Set(x uint32) {
-	// Do nothing
+	blockNumber := x / wordSize
+	bitPositionInBlock := wordSize - (x % wordSize) - 1
+	b.data[blockNumber] = b.data[blockNumber] | (1 << bitPositionInBlock)
 }
 
 func (b *uncompressedBitmap) Union(other *uncompressedBitmap) *uncompressedBitmap {
 	var data []uint64
+
 	return &uncompressedBitmap{
 		data: data,
 	}
