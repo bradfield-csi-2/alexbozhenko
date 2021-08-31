87a48ddceff45f6a3dbd23ed4a7012ba0abd92e8
Alex Bozhenko <alexbozhenko@gmail.com>
2021-08-31 16:39:43 +0300

    Add suboptimus code that uses messages(not tested yet)

--- 05_operating_systems/05_interprocess_comunication/suboptimus.c
+++ 05_operating_systems/05_interprocess_comunication/suboptimus.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <sys/sysinfo.h>
+#include <mqueue.h>
 #include "types.h"
 
 int START = 2, END = 20;
@@ -11,29 +13,27 @@ char *TESTS[] = {"brute_force", "brutish", "miller_rabin"};
 int num_tests = sizeof(TESTS) / sizeof(char *);
 
 // PLAN:
-// 1. create queues in suboptimus
-// 2. create request message with request for each algorithm, for each
-//    number in the range of numbers
-// 3. send messages to request queue
-// 4. fork and exec N primality.exe workers, where N=number of cpu cores
+//+ 1. create queues in suboptimus
+//+ 2. create request message with request for each algorithm, for each
+//     number in the range of numbers
+//+ 3. send messages to request queue
+//+ 4. fork and exec N primality.exe workers, where N=number of cpu cores
 // 5. receive request messages in primality.exe
 // 6. run primality algorithm and publish result to response queue in primality.exe
-// 7. receive response messages in suboptimus.exes
-// 8. (optionally sort before printing) print results
+//+ 7. receive response messages in suboptimus.exes
+// 8. (optionally sort before printing)
+//+ 9. print results
 
 int main(void)
 {
-  int testfds[num_tests][2];
-  int resultfds[num_tests][2];
-  int result, i;
   long n;
   pid_t pid;
 
-  for (i = 0; i < num_tests; i++)
-  {
-    pipe(testfds[i]);
-    pipe(resultfds[i]);
+  mqd_t request_queue = mq_open(request_queue_name, O_RDWR);
+  mqd_t response_queue = mq_open(response_queue_name, O_RDWR);
 
+  for (int i = 0; i < get_nprocs(); i++)
+  {
     pid = fork();
 
     if (pid == -1)
@@ -44,29 +44,30 @@ int main(void)
 
     if (pid == 0)
     {
-      // we are the child, connect the pipes correctly and exec!
-      close(testfds[i][1]);
-      close(resultfds[i][0]);
-      dup2(testfds[i][0], STDIN_FILENO);
-      dup2(resultfds[i][1], STDOUT_FILENO);
-      execl("primality.exe", "primality.exe", TESTS[i], (char *)NULL);
+      // we are the child, run primality.exe
+      execl("primality.exe", "primality.exe", (char *)NULL);
     }
-
-    // we are the parent
-    close(testfds[i][0]);
-    close(resultfds[i][1]);
   }
 
   // for each number, run each test
   for (n = START; n <= END; n++)
   {
-    for (i = 0; i < num_tests; i++)
+    for (int i = 0; i < NUM_ALGORITHMS; i++)
     {
-
       // we are the parent, so send test case to child and read results
-      write(testfds[i][1], &n, sizeof(n));
-      read(resultfds[i][0], &result, sizeof(result));
-      printf("%15s says %ld %s prime\n", TESTS[i], n, result ? "is" : "IS NOT");
+      struct request req = {.number = n, .alg = i};
+      mq_send(request_queue, (const char *)&req,
+              sizeof(req), (unsigned int)0);
     }
+  }
+  for (int i = 0; i <= NUM_ALGORITHMS * (END - START + 1); i++)
+  {
+    struct response *resp = {0};
+    mq_receive(response_queue, (char *)resp,
+               sizeof(struct response), (unsigned int)0);
+    printf("%15s says %ld %s prime\n",
+           ALGORITHMS_STRING[i],
+           resp->number,
+           resp->result ? "is" : "IS NOT");
   }
 }
--- 05_operating_systems/05_interprocess_comunication/types.h
+++ 05_operating_systems/05_interprocess_comunication/types.h
@@ -1,22 +1,46 @@
 #include <stdbool.h>
 
+// from https://stackoverflow.com/a/10966395
+#define FOREACH_ALGO(ALGO) \
+    ALGO(brute_force)      \
+    ALGO(brutish)          \
+    ALGO(miller_rabin)
+
+#define GENERATE_ENUM(ENUM) ENUM,
+#define GENERATE_STRING(STRING) #STRING,
+
 typedef enum
 {
-    brute_force,
-    brutish,
-    miller_rabin
-} algorithm;
+    FOREACH_ALGO(GENERATE_ENUM)
+} ALGORITHMS_ENUM;
+
+static const char *ALGORITHMS_STRING[] = {
+    FOREACH_ALGO(GENERATE_STRING)};
+
+const int NUM_ALGORITHMS = sizeof ALGORITHMS_STRING / sizeof *ALGORITHMS_STRING;
+
+const char *request_queue_name = "/suboptimus_request_queue";
+const char *response_queue_name = "/suboptimus_response_queue";
+
+// typedef enum
+// {
+//     brute_force,
+//     brutish,
+//     miller_rabin,
+//     // Please, add new algorithm before this comment
+//     num_algorithms
+// } algorithm;
 
 struct request
 {
     long number;
-    algorithm alg;
+    ALGORITHMS_ENUM alg;
 };
 
 struct response
 {
     long number;
-    algorithm alg;
+    ALGORITHMS_ENUM alg;
     double duration;
     bool result;
 };
