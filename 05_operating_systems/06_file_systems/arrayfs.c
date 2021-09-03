691ea86117b0c3758f6925e60873c8e9443a0d3d
Alex Bozhenko <alexbozhenko@gmail.com>
2021-09-03 18:28:40 +0300

    trying to figure out how to use global arrays, but at least it compiles

--- 05_operating_systems/06_file_systems/arrayfs.c
+++ 05_operating_systems/06_file_systems/arrayfs.c
@@ -4,36 +4,140 @@
 
 #include <fuse.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stddef.h>
 #include <assert.h>
 
-typedef char directory[DIR_LENGTH][255];
+char *array[ROOTDIR_LENGTH][DIR_LENGTH];
+
+// directory one = {"a", NULL, "b",
+// 				 NULL, NULL, NULL,
+// 				 NULL, NULL, NULL,
+// 				 "v"};
+// directory two = {"c", NULL, NULL,
+// 				 NULL, NULL, NULL,
+// 				 NULL, "d", NULL,
+// 				 "e"};
+// directory three = {NULL, NULL, NULL,
+// 				   "f", "g", "h",
+// 				   NULL, NULL, NULL,
+// 				   NULL};
+
+// directory array_tmp[ROOTDIR_LENGTH] = {NULL, NULL, NULL,
+// 										  one, two, three,
+// 										  NULL, NULL, NULL,
+// 										  NULL};
 
 static void *arrayfs_init(struct fuse_conn_info *conn,
 						  struct fuse_config *cfg)
 {
 	(void)conn;
 	cfg->kernel_cache = 1;
-	directory one = {"a", NULL, "b",
-					 NULL, NULL, NULL,
-					 NULL, NULL, NULL,
-					 "v"};
-	directory two = {"c", NULL, NULL,
-					 NULL, NULL, NULL,
-					 NULL, "d", NULL,
-					 "e"};
-	directory three = {NULL, NULL, NULL,
-					   "f", "g", "h",
-					   NULL, NULL, NULL,
-					   NULL};
-	directory root_dir[ROOTDIR_LENGTH] = {NULL, NULL, one,
-										  NULL, NULL, two,
-										  NULL, NULL, three, NULL};
+	array[0][0] = "a";
+	array[0][1] = NULL;
+	array[0][2] = NULL;
+	array[0][3] = NULL;
+	array[0][4] = NULL;
+	array[0][5] = NULL;
+	array[0][6] = NULL;
+	array[0][7] = NULL;
+	array[0][8] = NULL;
+	array[0][9] = NULL;
+	array[1][0] = NULL;
+	array[1][1] = NULL;
+	array[1][2] = NULL;
+	array[1][3] = NULL;
+	array[1][4] = NULL;
+	array[1][5] = NULL;
+	array[1][6] = NULL;
+	array[1][7] = NULL;
+	array[1][8] = NULL;
+	array[1][9] = NULL;
+	array[2][0] = NULL;
+	array[2][1] = "b";
+	array[2][2] = NULL;
+	array[2][3] = NULL;
+	array[2][4] = NULL;
+	array[2][5] = NULL;
+	array[2][6] = NULL;
+	array[2][7] = NULL;
+	array[2][8] = NULL;
+	array[2][9] = NULL;
+	array[3][0] = NULL;
+	array[3][1] = NULL;
+	array[3][2] = NULL;
+	array[3][3] = NULL;
+	array[3][4] = NULL;
+	array[3][5] = NULL;
+	array[3][6] = NULL;
+	array[3][7] = NULL;
+	array[3][8] = NULL;
+	array[3][9] = NULL;
+	array[4][0] = NULL;
+	array[4][1] = NULL;
+	array[4][2] = NULL;
+	array[4][3] = NULL;
+	array[4][4] = NULL;
+	array[4][5] = NULL;
+	array[4][6] = NULL;
+	array[4][7] = "c";
+	array[4][8] = NULL;
+	array[4][9] = NULL;
+	array[5][0] = NULL;
+	array[5][1] = NULL;
+	array[5][2] = NULL;
+	array[5][3] = NULL;
+	array[5][4] = NULL;
+	array[5][5] = NULL;
+	array[5][6] = NULL;
+	array[5][7] = NULL;
+	array[5][8] = NULL;
+	array[5][9] = NULL;
+	array[6][0] = NULL;
+	array[6][1] = NULL;
+	array[6][2] = NULL;
+	array[6][3] = NULL;
+	array[6][4] = NULL;
+	array[6][5] = NULL;
+	array[6][6] = NULL;
+	array[6][7] = NULL;
+	array[6][8] = NULL;
+	array[6][9] = NULL;
+	array[7][0] = NULL;
+	array[7][1] = NULL;
+	array[7][2] = NULL;
+	array[7][3] = NULL;
+	array[7][4] = NULL;
+	array[7][5] = NULL;
+	array[7][6] = NULL;
+	array[7][7] = NULL;
+	array[7][8] = NULL;
+	array[7][9] = NULL;
+	array[8][0] = NULL;
+	array[8][1] = "d";
+	array[8][2] = NULL;
+	array[8][3] = NULL;
+	array[8][4] = NULL;
+	array[8][5] = NULL;
+	array[8][6] = NULL;
+	array[8][7] = NULL;
+	array[8][8] = NULL;
+	array[8][9] = NULL;
+	array[9][0] = NULL;
+	array[9][1] = NULL;
+	array[9][2] = NULL;
+	array[9][3] = NULL;
+	array[9][4] = NULL;
+	array[9][5] = NULL;
+	array[9][6] = NULL;
+	array[9][7] = NULL;
+	array[9][8] = NULL;
+	array[9][9] = NULL;
 
-	return root_dir;
+	return array;
 }
 
 int parse_dir(const char *path)
@@ -42,7 +146,7 @@ int parse_dir(const char *path)
 	{
 		return -1;
 	}
-	return atoi(path[1]);
+	return path[1] - '0';
 }
 
 int parse_file(const char *path)
@@ -51,7 +155,7 @@ int parse_file(const char *path)
 	{
 		return -1;
 	}
-	return atoi(path[3]);
+	return path[3] - '0';
 }
 
 static int arrayfs_getattr(const char *path, struct stat *stbuf,
@@ -60,8 +164,7 @@ static int arrayfs_getattr(const char *path, struct stat *stbuf,
 	(void)fi;
 	int res = 0;
 	struct fuse_context fc = *fuse_get_context();
-	fc.private_data;
-	directory *data = (directory *)fc.private_data;
+	//char *arr[ROOTDIR_LENGTH][DIR_LENGTH] = fc.private_data;
 
 	memset(stbuf, 0, sizeof(struct stat));
 	int dir = parse_dir(path);
@@ -71,19 +174,20 @@ static int arrayfs_getattr(const char *path, struct stat *stbuf,
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2 + ROOTDIR_LENGTH;
 	}
-	else if (strchr(path + 1, "/") == NULL &&
-			 dir != -1 && data[dir] != NULL) // no more slashes in the path,
-											 // so it is a directory
+	else if (strchr(path + 1, '/') == NULL &&
+			 dir != -1 && array[dir] != NULL) // no more slashes in the path,
+											  // so it is a directory
 	{
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2;
 	}
-	else if (strchr(path + 1, "/") == 1 &&
-			 file != -1 && data[dir][file] != NULL) // found second slash, so it is a file
+	else if (((int)(strchr(path + 1, '/') - path) == 1) &&
+			 (file != -1) &&
+			 (array[dir][file] != NULL)) // found second slash, so it is a file
 	{
 		stbuf->st_mode = S_IFREG | 0444;
 		stbuf->st_nlink = 1;
-		stbuf->st_size = data[dir][file];
+		stbuf->st_size = strlen(array[dir][file]);
 	}
 	else
 		res = -ENOENT;
@@ -141,7 +245,7 @@ static int hello_read(const char *path, char *buf, size_t size, off_t offset,
 	return size;
 }
 
-static const struct fuse_operations hello_oper = {
+static const struct fuse_operations arrayfs_oper = {
 	.init = arrayfs_init,
 	.getattr = arrayfs_getattr,
 	.readdir = hello_readdir,
@@ -151,10 +255,17 @@ static const struct fuse_operations hello_oper = {
 
 int main(int argc, char *argv[])
 {
-	int ret;
-	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+	//struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+	printf("argc! %d\n", argc);
+	//printf("argv! %s\n", argv[3])	;
+	printf("here!\n");
+	printf("here!\n");
+	printf("!!!%p\n", &arrayfs_oper);
+	printf("there!\n");
 
-	ret = fuse_main(args.argc, args.argv, &hello_oper, NULL);
-	fuse_opt_free_args(&args);
-	return ret;
+	return fuse_main(argc,
+					 argv,
+					 &arrayfs_oper,
+					 NULL);
+	//fuse_opt_free_args(&args);
 }
