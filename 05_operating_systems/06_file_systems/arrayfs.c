dd9b05b63a4166f4dc262a71fa344f030408a3f5
Alex Bozhenko <alexbozhenko@gmail.com>
2021-09-03 16:33:31 +0300

    add getatter function(not tested)

--- 05_operating_systems/06_file_systems/arrayfs.c
+++ 05_operating_systems/06_file_systems/arrayfs.c
@@ -1,4 +1,6 @@
 #define FUSE_USE_VERSION 31
+#define ROOTDIR_LENGTH 10
+#define DIR_LENGTH 10
 
 #include <fuse.h>
 #include <stdio.h>
@@ -8,13 +10,48 @@
 #include <stddef.h>
 #include <assert.h>
 
-static void *hello_init(struct fuse_conn_info *conn,
-						struct fuse_config *cfg)
+typedef char directory[DIR_LENGTH][255];
+
+static void *arrayfs_init(struct fuse_conn_info *conn,
+						  struct fuse_config *cfg)
 {
 	(void)conn;
 	cfg->kernel_cache = 1;
-	//TODO allocate array and return it!
-	return NULL;
+	directory one = {"a", NULL, "b",
+					 NULL, NULL, NULL,
+					 NULL, NULL, NULL,
+					 "v"};
+	directory two = {"c", NULL, NULL,
+					 NULL, NULL, NULL,
+					 NULL, "d", NULL,
+					 "e"};
+	directory three = {NULL, NULL, NULL,
+					   "f", "g", "h",
+					   NULL, NULL, NULL,
+					   NULL};
+	directory root_dir[ROOTDIR_LENGTH] = {NULL, NULL, one,
+										  NULL, NULL, two,
+										  NULL, NULL, three, NULL};
+
+	return root_dir;
+}
+
+int parse_dir(const char *path)
+{
+	if (strlen(path) == 1)
+	{
+		return -1;
+	}
+	return atoi(path[1]);
+}
+
+int parse_file(const char *path)
+{
+	if (strlen(path) < 4)
+	{
+		return -1;
+	}
+	return atoi(path[3]);
 }
 
 static int arrayfs_getattr(const char *path, struct stat *stbuf,
@@ -24,24 +61,29 @@ static int arrayfs_getattr(const char *path, struct stat *stbuf,
 	int res = 0;
 	struct fuse_context fc = *fuse_get_context();
 	fc.private_data;
+	directory *data = (directory *)fc.private_data;
 
 	memset(stbuf, 0, sizeof(struct stat));
+	int dir = parse_dir(path);
+	int file = parse_file(path);
 	if (strcmp(path, "/") == 0)
 	{
 		stbuf->st_mode = S_IFDIR | 0755;
-		stbuf->st_nlink = 2; //TODO + num of elems on first level
+		stbuf->st_nlink = 2 + ROOTDIR_LENGTH;
 	}
-	else if (strchr(path + 1, "/") == NULL) // no more slashes in the path,
-											// so it is a directory
+	else if (strchr(path + 1, "/") == NULL &&
+			 dir != -1 && data[dir] != NULL) // no more slashes in the path,
+											 // so it is a directory
 	{
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2;
 	}
-	else if (strchr(path + 1, "/") == 1) // found second slash, so it is a file
+	else if (strchr(path + 1, "/") == 1 &&
+			 file != -1 && data[dir][file] != NULL) // found second slash, so it is a file
 	{
 		stbuf->st_mode = S_IFREG | 0444;
 		stbuf->st_nlink = 1;
-		stbuf->st_size = strlen("hello"); //TODO length of the string contents
+		stbuf->st_size = data[dir][file];
 	}
 	else
 		res = -ENOENT;
@@ -100,7 +142,7 @@ static int hello_read(const char *path, char *buf, size_t size, off_t offset,
 }
 
 static const struct fuse_operations hello_oper = {
-	.init = hello_init,
+	.init = arrayfs_init,
 	.getattr = arrayfs_getattr,
 	.readdir = hello_readdir,
 	.open = hello_open,
