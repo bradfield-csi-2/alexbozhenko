6b18905db143ee749993b13ac8567e743e295041
Alex Bozhenko <alexbozhenko@gmail.com>
2021-11-11 15:21:10 +0300

    Refactor hashRing to store array of stucts containing hash along with nodeName
    This allows to return both nodeName and nodeValue for a given key

--- 07_distributed_systems/01_intro/server/consitentHash.go
+++ 07_distributed_systems/01_intro/server/consitentHash.go
@@ -35,7 +35,7 @@ type consistentHashRing struct {
 }
 
 func NewConsistentHashRing(replicas, nodeNumber int) *consistentHashRing {
-	rHashes := make([][md5.Size]byte, 0)
+	rHashes := make([]replicaHashWithNodeName, 0)
 	return &consistentHashRing{
 		replicas:       replicas,
 		replicasHashes: rHashes,
@@ -43,35 +43,41 @@ func NewConsistentHashRing(replicas, nodeNumber int) *consistentHashRing {
 	}
 }
 
+func replicaHash(nodeName string, replicaNumber int) [md5.Size]byte {
+	return md5.Sum([]byte(fmt.Sprintf("%s:%d", nodeName, replicaNumber)))
+}
+
 func (c *consistentHashRing) addNode(nodeName string, nodeValue string) {
-	for i := 0; i < c.replicas; i++ {
-		replicaHashArray := md5.Sum([]byte(fmt.Sprintf("%s:%d", nodeName, i)))
-		replicaHash := replicaHashArray[:]
+	for replicaNumber := 0; replicaNumber < c.replicas; replicaNumber++ {
+		replicaHashArray := replicaHash(nodeName, replicaNumber)
+		replicaHash := replicaHashWithNodeName{
+			hash:     replicaHashArray,
+			nodeName: nodeName,
+		}
 		index := sort.Search(len(c.replicasHashes),
 			func(i int) bool {
-				res := bytes.Compare(c.replicasHashes[i][:], replicaHash)
+				res := bytes.Compare(c.replicasHashes[i].hash[:], replicaHash.hash[:])
 				return res >= 0
 			})
 
-		c.replicasHashes = append(append(c.replicasHashes[:index][:], replicaHashArray),
+		c.replicasHashes = append(append(c.replicasHashes[:index][:],
+			replicaHash),
 			c.replicasHashes[index:]...)
-
-		c.nodes[string(replicaHash)] = nodeValue
 	}
+	c.nodes[nodeName] = nodeValue
 }
 
 func (c *consistentHashRing) deleteNode(nodeName string) {
-	for i := 0; i < c.replicas; i++ {
-		replicaHashArray := md5.Sum([]byte(fmt.Sprintf("%s:%d", nodeName, i)))
-		replicaHash := replicaHashArray[:]
+	for replicaNumber := 0; replicaNumber < c.replicas; replicaNumber++ {
+		replicaHashArray := replicaHash(nodeName, replicaNumber)
 		index := sort.Search(len(c.replicasHashes),
 			func(i int) bool {
-				return (bytes.Equal(c.replicasHashes[i][:], replicaHash))
+				return (bytes.Equal(c.replicasHashes[i].hash[:], replicaHashArray[:]))
 			})
 		if index == len(c.replicasHashes) {
 			panic(fmt.Sprintf("BUG! %s is not present in the hash ring", nodeName))
 		}
-		c.replicasHashes = append(c.replicasHashes[:index][:], c.replicasHashes[index+1:]...)
+		c.replicasHashes = append(c.replicasHashes[:index], c.replicasHashes[index+1:]...)
 	}
 	delete(c.nodes, nodeName)
 }
@@ -80,20 +86,23 @@ func (c *consistentHashRing) deleteNode(nodeName string) {
 func (c *consistentHashRing) getNode(key string) (nodeName string, nodeValue string) {
 	keyHash := md5.Sum([]byte(key))
 	var index int
+	// find index of the first replica hash, where
+	// replicaHash >= keyHash
 	index = sort.Search(len(c.replicasHashes),
 		func(i int) bool {
-			res := bytes.Compare(c.replicasHashes[i][:], keyHash[:])
+			res := bytes.Compare(c.replicasHashes[i].hash[:], keyHash[:])
 			return res >= 0
 		})
 
 	if index == len(c.replicasHashes) {
 		// Special case to connect end of the hash function range
 		// to the beginning to create a "ring"
+		// In other words, is the first replica responsible for
+		// the tail of the hash ring
 		index = 0
 	}
-	replicaHash := c.replicasHashes[index]
-	nodeName = string(c.replicasHashes[index][:])
-	nodeValue = c.nodes[string(replicaHash[:])]
+	nodeName = c.replicasHashes[index].nodeName
+	nodeValue = c.nodes[nodeName]
 
 	return
 
