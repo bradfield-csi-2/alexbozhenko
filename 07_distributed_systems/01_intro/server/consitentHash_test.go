02c9a50bec6ce136fd1abe061e880654ebfd502f
Alex Bozhenko <alexbozhenko@gmail.com>
2021-11-11 16:15:18 +0300

    Fix consistentHash test(but found out a bug)

--- 07_distributed_systems/01_intro/server/consitentHash_test.go
+++ 07_distributed_systems/01_intro/server/consitentHash_test.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
-	"sort"
-	"strings"
 	"testing"
 	"time"
 
-	"gonum.org/v1/gonum/stat"
+	"github.com/stretchr/testify/assert"
 )
 
-const NUM_REPLICAS int = 100
-const NUM_KEYS_TO_ADD int = 10_000_000
-const MAX_VALUE int = 10_000_000
+const NUM_REPLICAS int = 10
+const NUM_KEYS_TO_ADD int = 1_000_000
+const MAX_VALUE int = 100_000_000
 const NUM_NODES int = 10
 
 // func stdDev(m map[string]int) {
@@ -27,36 +26,36 @@ const NUM_NODES int = 10
 func TestConsistentHashRing(t *testing.T) {
 	hashRing := NewConsistentHashRing(NUM_REPLICAS, NUM_NODES)
 	for i := 0; i < NUM_NODES; i++ {
-		hashRing.addNode(fmt.Sprintf("name%d", i), fmt.Sprintf("value%d", i))
+		hashRing.addNode(fmt.Sprintf("name%03d", i), fmt.Sprintf("value%d", i))
 	}
-	nodesToKeysCounter := make(map[string]int)
+	nodesToKeysCounter := make(map[string]float64)
+
 	rand.Seed(time.Now().UnixNano())
 
 	for i := 0; i < NUM_KEYS_TO_ADD; i++ {
 		nodeName, _ := hashRing.getNode(fmt.Sprintf("%d", rand.Intn(MAX_VALUE)))
 		nodesToKeysCounter[nodeName]++
 	}
-	// slice where s[i] = n,
-	// where n = number of nodes with i keys
-	// in other words:
-	// this many keys seen on how many nodes
-	var nodesCounterFloatValues []float64
-	numBuckets := NUM_KEYS_TO_ADD / (NUM_NODES * NUM_REPLICAS)
-	sortedNodesDistribution := make([]int, NUM_KEYS_TO_ADD)
-	for _, count := range nodesToKeysCounter {
-		nodesCounterFloatValues = append(nodesCounterFloatValues, float64(count))
-		sortedNodesDistribution[count/numBuckets]++
+	t.Log(fmt.Sprint(nodesToKeysCounter))
+	// stdDev := stat.PopStdDev(nodesCounterFloatValues, nil)
+	var mean float64
+	for _, value := range nodesToKeysCounter {
+		mean += value
 	}
-	for numHits, numberOfNodesWithThisManyHits := range sortedNodesDistribution {
-		if numberOfNodesWithThisManyHits != 0 {
-			fmt.Printf("%d: %s\n", numHits, strings.Repeat("#", numberOfNodesWithThisManyHits))
-		}
+	var observedNodesNumber int = len(nodesToKeysCounter)
+	assert.Equal(t, NUM_NODES, observedNodesNumber,
+		"Observed number of nodes must be"+
+			"equal to the expected number of nodes")
+	mean /= float64(observedNodesNumber)
+	var standardDeviation float64
+	for _, value := range nodesToKeysCounter {
+		standardDeviation += math.Pow(value-mean, 2)
 	}
-	sort.Float64s(nodesCounterFloatValues)
-	t.Log(nodesCounterFloatValues)
-	stdDev := stat.PopStdDev(nodesCounterFloatValues, nil)
-	if stdDev > 0.2 {
-		t.Errorf("Nodes deviation: %f, want: <= 0.2", stdDev)
+	standardDeviation = math.Pow(standardDeviation/float64(observedNodesNumber), 0.5)
+	t.Log("mean = ", mean)
+	t.Log("expected = ", NUM_KEYS_TO_ADD/NUM_NODES)
+	if standardDeviation > 0.2 {
+		t.Errorf("Nodes deviation: %f, want: <= 0.2", standardDeviation)
 	}
 	// for _, tt := range tests {
 	// 	t.Run(tt.name, func(t *testing.T) {
